4be7178db0d29396b0c90e37ca1978b116fd7214
kwiaciu <bkwiatek3@gmail.com>
2020-02-23 14:49:51 +0100

    added notes and backend support for adding

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const proxy = require('express-http-proxy');
+const bodyParser = require('body-parser');
 
 const PORT = 3210;
 
 const app = express();
+const NOTES_URL = '/notes';
 
 const isProduction = !!process.env.WEBPACK_PRODUCTION;
+
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 
 // const frontend
 
 //serwowanie statycznych plików
 // app.get('*', express.static('./dist'));
 
-app.get('*', isProduction
-    ? express.static('./dist')
-    : proxy('http://localhost:8081')
-);
 
 
 // app.get('*', function(req, res) {
@@ -24,10 +33,36 @@ app.get('*', isProduction
 //     })
 // })
 
+const notes = ['one note', 'two notes', 'three notes']
+
+app.get(NOTES_URL, (req,res) => {
+    res.json({
+        notes: notes
+    })
+});
+
+app.post(NOTES_URL, (req,res) => {
+    const note = req.body.note;
+    notes.push(note);
+    res.json({
+        value: note
+    })
+});
+
+app.delete(NOTES_URL, (req,res) => {
+    res.json({
+        status: 'deleted'
+    })
+});
+
+app.get('*', isProduction
+    ? express.static('/dist')
+    : proxy('http://localhost:8081')
+);
+
+
 app.listen(PORT, function() {
     // console.log(`listening on port http://localhost:${PORT}`);
     console.log(`listening on port \u001b[36;1m http://localhost:${PORT} \x1b[0m`);
-    
-
 
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,40 @@
 console.log('Hello!!')
+
+const NOTES_URL = '/notes';
+
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.getElementById('content');
+    const todos = document.getElementById('todos');
+    const todo = document.getElementById('todo');
+
+    todos.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const note = todo.value;
+        todo.value = ''; 
+        addNote(note);
+        fetch(NOTES_URL, {
+            method: 'post',
+            headers: headers,
+            body: JSON.stringify({note: note})
+        })
+    });
+
+    function addNote(note) {
+        const element = document.createElement('li');
+        element.innerText = note;
+        element.className = 'note';
+        content.appendChild(element);
+        
+    }
+
+    fetch(NOTES_URL).then(res => res.json()).then(body => {
+        content.innerHTML = '';
+        body.notes.forEach((note) => addNote(note));
+    });
+
+})
