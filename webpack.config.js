ad643846847ce156c077587a8f796e0beac9d649
kwiaciu <bkwiatek3@gmail.com>
2020-02-23 11:05:54 +0100

    Webpack added and configured

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+// const HTMLWebpackPlugin = require('html-webpack-plugin'); 
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// const isProduction = process.env.WEBPACK_PRODUCTION;
+
+
+// const htmlPluginConfig = {template: './src/index.html'};
+// const htmlPlugin =new HTMLWebpackPlugin(htmlPluginConfig);
+
+
+// const copyPlugin = new HTMLWebpackPlugin([{ from: './src/style.css' }]);
+
+// module.exports = {
+//     entry: './src/main.js',
+//     output: {
+//         path: __dirname + '/dist',
+//         filename: '[name].js'
+//     },
+
+//     plugins: [htmlPlugin, copyPlugin],
+
+//     mode: isProduction ? 'production': 'development'
+// };
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const isProduction = !!process.env.WEBPACK_PRODUCTION;
+
+console.log('isProduction:', typeof isProduction);
+
+const htmlPluginConfig = { template: './src/index.html' };
+const htmlPlugin = new HtmlWebpackPlugin(htmlPluginConfig)
+
+const copyPlugin = new CopyWebpackPlugin([{
+    from: './src/style.css'
+}]);
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: '[name].js'
+    },
+    plugins: [htmlPlugin, copyPlugin],
+    mode: isProduction ? 'production' : 'development'
+};
